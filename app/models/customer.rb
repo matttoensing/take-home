c1165a69fc13a9734f15284e8b24f2396de24083
Matt Toensing <matthew.toensing@gmail.com>
2021-11-05 07:10:17 -0700

    Add email uniqueness validation to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,6 @@ class Customer < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :address, presence: true
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'relationships' do
-    # it { should have_many(:customer_subscriptions) }
     it { should have_many(:subscriptions) }
   end
 
@@ -11,5 +10,6 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:address) }
+    it { should validate_uniqueness_of(:email) }
   end
 end
